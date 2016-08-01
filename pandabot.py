ca32af60e9c4bb828b554856be2af49732b5bf47
neilpatrick02 <neilpatrick02@gmail.com>
2016-08-01 17:56:07 +0800

    pandabot.py
    - Changed webhook url
    - Changed woe ID from Philippines to Taiwan
    - Changed token to a mock-token (patterned token n = number, a=small letter, A=capital letter) token=xoxb-64974996416-NiosKLeD2r0GUStwLWXaa7Ya

--- pandabot.py
+++ pandabot.py
@@ -2,17 +2,17 @@ import tweepy
 import requests
 import json
 
-url = 'https://hooks.slack.com/services/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' #ISGHP3-TEAMPANDA.SLACK.COM
+url = 'https://hooks.slack.com/services/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'
 
 consumer_key = 'Zk8So9v4o4nFLaB6mvi4E8SXK'
 consumer_secret = 'zW0MeLcCshDEG4iHld6wRciZPD4nyZRRK7tv6H5zmTRMMnJg1K'
-access_token = 	'278088267-I0sKpkMq1jOPGGUScJVBhtkoDeTRRAaSI9dEQe2R'
+access_token = 	'aaaa-nnnnnnnnnnn-AaaaAAaAnanAAAaaAAAaanAa'
 access_token_secret = 'vQvu6x8QhTtDdkbXIALlkbqvU9FKRqnoPFARQCR9xCv6V'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
-trends1 = api.trends_place(1199477) #MANILA
+trends1 = api.trends_place(23424971) #TAIWAN
 data = trends1[0] 
 trends = data['trends']
 names = [trend['name'] for trend in trends]
