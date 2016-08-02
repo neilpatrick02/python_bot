288c4fecee9a14ab7d899145a34112a775608c95
neilpatrick02 <neilpatrick02@gmail.com>
2016-08-02 16:28:05 +0800

    pandabot.py
    Added a string "Top Trends in Manila: " and a corresponding date

--- pandabot.py
+++ pandabot.py
@@ -1,28 +1,37 @@
 import tweepy
 import requests
 import json
+import datetime
 
 url = 'https://hooks.slack.com/services/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'
 
-consumer_key = 'Zk8So9v4o4nFLaB6mvi4E8SXK'
-consumer_secret = 'zW0MeLcCshDEG4iHld6wRciZPD4nyZRRK7tv6H5zmTRMMnJg1K'
-access_token = 	'aaaa-nnnnnnnnnnn-AaaaAAaAnanAAAaaAAAaanAa'
+
+consumer_key = 'OKaaZZ8EIo1jme4FCpD2gdh9t'
+consumer_secret = 'ksLwFur4MFRDCiZ9jzBGXDs1A4NPqE3iXa1Fi3t1GSG5x5hOOI'
+access_token = 	'nnnnnnnnn-AnaAaaAanaAAAAAAaAAAaaaaAaAAAAaAAnaAAanA'  #278088267-I0sKpkMq1jOPGGUScJVBhtkoDeTRRAaSI9dEQe2R
 access_token_secret = 'vQvu6x8QhTtDdkbXIALlkbqvU9FKRqnoPFARQCR9xCv6V'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 trends1 = api.trends_place(1199477) #MANILA
-data = trends1[0] 
+data = trends1[0]
 trends = data['trends']
 names = [trend['name'] for trend in trends]
 
-hashtags = ""
+date = datetime.date.today()
+
+hashtags = "Top Trends in Manila: " + str(date.strftime('%b. %d, %Y')) + "\n"
+ctr = 1
 for i in names[:10]:
-	hashtags += i + "\n" 
-	
+	hashtags += str(ctr) + ". " + i + "\n"
+	ctr += 1
+
 payload = {'text' : hashtags}
+
+# print hashtags
 
 r = requests.post(url, data=json.dumps(payload))
 r.text
 r.status_code
+
